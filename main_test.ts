6d2906357e3eeae0bbb833b4aa45e7e45a7b54f6
antklim <antklim@gmail.com>
2022-10-04 08:06:08 +1100

    Moved dependencies to deps.ts

--- main_test.ts
+++ main_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.158.0/testing/asserts.ts";
+import { assertEquals } from "./deps.ts";
 import { add } from "./main.ts";
 
 Deno.test(function addTest() {
